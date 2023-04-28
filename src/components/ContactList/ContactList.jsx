e66bcf006e47f33434ddbc46849f4bb0fd83ee4f
TetianaChernenko <113249605+TetianaChernenko@users.noreply.github.com>
2023-04-28 12:05:38 +0300

    зберігання в localStorage

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import css from "./ContactList.module.css";
+
+export const ContactList = ({ contacts, onDeleteContact }) => {
+    return (
+        <ul className={css.contactsList}>
+            {contacts.map(contact => (
+                <li className={css.contactsItem} key={contact.id}>
+                    <span>{contact.name}:</span> <span>{contact.number}</span>
+                    <button className={css.contactsBtn} type="button" onClick={() => onDeleteContact(contact.id)}>Delete</button>
+                </li>
+            ))}
+        </ul>
+    )
+};
+
+ContactList.prototype = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+	onDeleteContact: PropTypes.func.isRequired,
+}
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import css from "./Filter.module.css";
+import PropTypes from 'prop-types';
+
+
+export const Filter = ({value, onChange}) => {
+    return (
+    <label className={css.label}>
+			Find contacts by name
+			<input className={css.input} type="text" name="filter" value={value} onChange={onChange}/>
+	</label>
+)
+}
+
+Filter.prototype = {
+	value: PropTypes.string,
+	onChange: PropTypes.func.isRequired,
+}
